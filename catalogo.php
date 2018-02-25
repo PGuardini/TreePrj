5ba56058accecdaa33d5473b8721cea5e50628f4
Pedro Augusto <pedro.canteli2001@gmail.com>
2018-02-25 10:35:54 -0300

    exclusão de fontes que não apareciam devido ao semantic, inclusão da página catalogo

---
+++ catalogo.php
@@ -0,0 +1,128 @@
+<php 
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="css/semantic/semantic.min.css">
+    <link rel="stylesheet" href="css/canteli.css">
+    <title>CulTrees</title>
+</head>
+<body>
+    <h2 class="ui horizontal divider header tituloPag">
+        Catálogo
+    </h2>
+
+    <div class="ui stackable centered four column grid">
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="ui stackable centered four column grid">
+
+         <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="ui stackable centered four column grid">
+       
+        <div class="column">
+                <div class="ui piled segment containerCatalogo">
+                    <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                    <a href="">
+                        <div class="middleCatalogo">
+                            <div class="textCatalogo">John Doe</div>
+                        </div>
+                    </a>
+                </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="ui piled segment containerCatalogo">
+                <img class="ui image imageCatalogo" src="imagens/catalogo/conifera.jpg">
+                <a href="">
+                    <div class="middleCatalogo">
+                        <div class="textCatalogo">John Doe</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
