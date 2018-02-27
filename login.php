67e77eaffd660e6b7f9c1f0cc22cc771f135688e
Victor Hugo <victorhugonass@gmail.com>
2018-02-26 21:15:51 -0300

    página de login pronta

--- login.php
+++ login.php
@@ -1,7 +1,23 @@
 <?php
     include "html/cabecalho.html";
 ?>
-    <p>it works!</p>
+<div class="ui grid">
+    <div id="margemCincoPorcento" class="three column row">
+        <div class="column"></div>
+        <form class="column ui form">
+            <div class="field">
+                <label>Username</label>
+                <input type="text" name="first-name" placeholder="Username">
+            </div>
+            <div class="field">
+                <label>Password</label>
+                <input type="password" name="last-name" placeholder="Password">
+            </div>
+            <div class="field"></div>
+            <button id="botaoVerde" class="ui green button" type="submit">Log In</button>
+        </form>
+    </div>
+</div>
 
 <?php
     include "html/rodape.html";
