8be9a6f3715f1a7f5ffb8759c0591fdb9309086f
Pedro Gabriel Gabiatti Guardini <pgabiattiguardini@gmail.com>
2018-02-28 21:23:35 -0300

    problema cadastro arvore resolvido

--- cadastroArvore.php
+++ cadastroArvore.php
@@ -52,16 +52,6 @@
             <input placeholder="Origem" type="text">
         </div>
     </div>
-<<<<<<< HEAD:cadastro.php
-    <div class="nine fields">
-        <div class='field'></div>
-        <div class='field'></div>
-        <div class='field'></div>
-        <div class='field'></div>
-        <div class='field'>
-            <button class="ui green button" type="submit">Enviar</button>
-        </div>
-=======
     <div class="ui one column centered grid">
         <div class="column"></div>
             <label for="file" class="ui icon button large">
@@ -73,7 +63,6 @@
     <div class="ui one column centered grid">
         <div class="column"></div>
             <button class="ui green button center aligned" type="submit">Adicionar Árvore</button>
->>>>>>> 72d48861f6ebbeeaba82020daec23c2e6a972113:cadastroArvore.php
     </div>
 </div>
 
