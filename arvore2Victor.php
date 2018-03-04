b7817b2840f4a3e83f79978d75f20fc9fba31b9d
Victor Hugo <victorhugonass@gmail.com>
2018-03-04 17:04:51 -0300

    apenas dei a forma das páginas de detalhamento de árvore, não coloquei nenhuma informação

--- arvore2Victor.php
+++ arvore2Victor.php
@@ -1,7 +1,115 @@
 <?php
     include "html/cabecalho.html";
 ?>
-it works2!
+ <div class="espacadorTitulo"></div>
+
+<h2 class="ui horizontal divider header tituloPag">
+    Ipê
+</h2>
+
+<div class="espacadorTitulo"></div>
+
+<div class="ui stackable sixteen column grid espacoColunas">
+    <div class="column"></div>
+
+
+     <div class="eight wide column">
+         <div class="ui center aligned piled segment">
+              <h2 class="ui center aligned header tituloSegmento">História</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor arcu a mauris fringilla, eu aliquam orci dignissim. Aenean semper orci in tellus ultricies interdum. Maecenas eget risus eu lorem fermentum eleifend bibendum in nisi. Aliquam cursus nisl et blandit dapibus. Nulla nec sagittis eros. Vivamus viverra elementum nisi vitae aliquet. Mauris eu cursus massa, quis imperdiet ante. Fusce in nunc libero. In interdum cursus imperdiet. Nulla facilisi. Maecenas interdum lectus massa, sed commodo ligula ultrices sit amet. Sed fringilla molestie elit, quis finibus quam blandit quis.</p>
+
+              <p>Nullam ullamcorper sapien et libero consequat dapibus. In mollis magna vel ligula aliquam, id sollicitudin eros mattis. Pellentesque blandit nibh a quam efficitur, eu molestie turpis hendrerit. Fusce ut enim est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed venenatis purus lacus, nec malesuada urna finibus vestibulum. Curabitur in nisl eu ex feugiat condimentum ac et purus. Duis vulputate tortor in nisl pharetra, id porta ligula facilisis. Ut tincidunt sapien sit amet elementum consectetur. Duis ac augue ut magna iaculis gravida at vel erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; </p>
+
+            <p>Morbi et leo diam. Integer iaculis elementum tincidunt. Nulla quis purus eu leo hendrerit venenatis sed quis ex. Sed nisi arcu, fringilla vel enim scelerisque, auctor venenatis velit. Vivamus at tincidunt nulla. Aenean varius eu velit eu tincidunt. Etiam auctor risus sit amet convallis commodo. In tempus euismod tincidunt. Donec ullamcorper massa imperdiet laoreet placerat. Sed porttitor orci congue, vulputate lectus in, sollicitudin arcu.</p>
+        </div>
+    </div>
+
+    <div class="column"></div>
+
+     <div class="five wide column">
+        <div class="ui piled segment imagemDetalha">
+            <img class="ui huge image imagemDetalha" src="imagens/detalhaArvore/ipe.jpg"/>  
+        </div>
+    </div>
+
+    <div class="column"></div>
+    
+</div>
+
+<div class="divisorDetalha"></div>
+
+<div class="ui sixteen column grid">
+    
+    <div class="column"></div>
+
+    <div class="six wide column">
+        <div class="ui styled fluid accordion">
+            <div class="title active">
+                <i class="dropdown icon"></i>
+                Como cuidar de um ipê?
+            </div>
+            <div class="content active">
+                <p class="transition visible" style="display: block !important;">Ipê tem 100 espécies e é a árvore ornamental mais plantada no Brasil. Já até propuseram que o ipê seja a flor nacional, sendo que o pau-brasil é a árvore nacional. O projeto que trata do assunto se arrasta há décadas, em Brasília. O ipê já flor símbolo nacional em El Salvador, Equador, Venezuela e Paraguai. Ele tinge a paisagem do México até a Patagônia. Ao todo, são cem espécies esparramadas pelas Américas.</p>
+                <p class="transition visible" style="display: block !important;">Assim como os outros ipês, se desenvolve bem em local ensolarado. É pouco exigente quanto à fertilidade da terra, mas não aprecia solo pesado, que acumule muita água. Torna-se muito resistente à seca depois que está bem estabelecido, portanto, irrigue bem no primeiro ano de implantação.  </p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Posso encontrar um ipê em quais variações?
+            </div>
+            <div class="content">
+                <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Onde comprar uma muda?
+            </div>
+            <div class="content">
+                <p class="transition hidden">Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
+                <p class="transition hidden">A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="eight wide column">
+        <div class="ui segment">
+            <div class="ui comments">
+              <div class="comment">
+                <a class="avatar imagemUsuario">
+                  <img src="imagens/detalhaArvore/avatar.jpg">
+                </a>
+                <div class="content">
+                  <a class="author">Antônio</a>
+                  <div class="metadata">
+                    <div class="date">2 dias atrás</div>
+                  </div>
+                  <div class="text">
+                    Amo esta árvore! 
+                  </div>
+                  <div class="actions">
+                    <a class="reply active">Responder</a>
+                  </div>
+                  <form class="ui reply form">
+                    <div class="field">
+                      <textarea></textarea>
+                    </div>
+                    <div class="ui primary submit labeled icon button botaoEnviar">
+                      <i class="icon edit"></i> Adicionar Resposta
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="column"></div>
+
+</div>
+
+
+
+
+<div class="espacadorRodape"></div> 
 <?php
     include "html/rodape.html";
 ?>
