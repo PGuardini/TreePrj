7409dc4135f19ec8cad32a3d35d0da9836b43402
Victor Hugo <victorhugonass@gmail.com>
2018-03-05 22:55:45 -0300

    detalhaMacieira com todos os dados, só falta fazer funcionar o accordion

---
+++ detalhaAcacia.php
@@ -0,0 +1,152 @@
+<?php
+    include 'html/cabecalho.html';
+?>
+
+    <div class="espacadorTitulo"></div>
+
+    <h2 class="ui horizontal divider header tituloPag">
+        Pau Formiga
+    </h2>
+
+    <div class="espacadorTitulo"></div>
+
+    <div class="ui stackable sixteen column grid espacoColunas">
+        <div class="column"></div>
+
+
+         <div class="eight wide column">
+        	 <div class="ui center aligned piled segment">
+                  <h2 class="ui center aligned header tituloSegmento">Informações</h2>
+                  
+                  <div class="ui divider"></div>
+                
+                <h4 class="ui header">
+                <i class="book icon"></i>
+                <div class="content">
+                    Nome Científico: <em>Triplaris americana</em>
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="globe icon"></i>
+                <div class="content">
+                    Nome Popular: Pau-formiga
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="map signs icon"></i>
+                <div class="ui left aligned content">
+                    Bioma: Locais de clima Equatorial, Subtropical e Tropical
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="map marker alternate icon"></i>
+                <div class="ui left aligned content">
+                    Ocorrência:  região amazônica e Centro-Oeste do Brasil.
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="lemon icon"></i>
+                <div class="ui left aligned content">
+                   Frutífera: Não. 
+                </div>
+                </h4>
+
+            </div>
+        </div>
+
+        <div class="column"></div>
+
+         <div class="five wide column">
+            <div class="ui piled segment imagemDetalha">
+                <img class="ui huge image imagemDetalha" src="imagens/catalogo/pauformiga.jpg"/>  
+            </div>
+
+        </div>
+
+        <div class="column"></div>
+        
+	</div>
+
+	<div class="divisorDetalha"></div>
+
+    <div class="ui sixteen column grid">
+        
+        <div class="column"></div>
+
+        <div class="six wide column">
+            <div class="ui styled fluid accordion">
+                <div class="title active">
+                    <i class="dropdown icon"></i>
+                    Quais as diferenças de cores?
+                </div>
+                <div class="content active">
+                    <p class="transition visible textoJustificado" style="display: block !important;">Por ser dioica, possui plantas femininas e masculinas que se diferenciam nas cores da flor. A feminina, de rosa intenso, é vistosa em relação à masculina, mais apagada. Elas podem ser vistas de agosto a outubro.</p>
+                    <p class="transition visible textoJustificado" style="display: block !important;">Também conhecida por pau-de-novato, novateiro-de-mato-grosso ou formigueiro, esta árvore é empregada também no paisagismo, tendo em vista a beleza de suas flores. Apresenta preferência por solos muito úmidos ou alagados. De crescimento rápido, aos dois anos já atinge três metros de altura.</p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    Quais as cidades que utilizam esta árvore para urbanismo?
+                </div>
+                <div class="content">
+                    <p class="transition hidden">.</p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    As flores aparecem em qual época?
+                </div>
+                <div class="content">
+                    <p class="transition hidden">.</p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    Onde Comprar
+                </div>
+            </div>
+        </div>
+
+        <div class="eight wide column">
+	    	<div class="ui segment">
+			    <div class="ui comments">
+				  <div class="comment">
+				    <a class="avatar imagemUsuario">
+				      <img src="imagens/detalhaArvore/avatar3.jpg">
+				    </a>
+				    <div class="content">
+				      <a class="author">Kylie</a>
+				      <div class="metadata">
+				        <div class="date">Hoje</div>
+				      </div>
+				      <div class="text">
+				        Sempre que vejo esta árvore me encanto com a sua beleza.
+				      </div>
+				      <div class="actions">
+				        <a class="reply active">Responder</a>
+				      </div>
+				      <form class="ui reply form">
+				        <div class="field">
+				          <textarea></textarea>
+				        </div>
+				        <div class="ui primary submit labeled icon button botaoEnviar">
+				          <i class="icon edit"></i> Adicionar Resposta
+				        </div>
+				      </form>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>
+
+        <div class="column"></div>
+
+    </div>
+
+
+    <div class="espacadorInferior"></div> 
+
+<?php
+    include 'html/rodape.html';
+?>
---
+++ detalhaMacieira.php
@@ -0,0 +1,155 @@
+<?php
+    include 'html/cabecalho.html';
+?>
+
+    <div class="espacadorTitulo"></div>
+
+    <h2 class="ui horizontal divider header tituloPag">
+        Macieira
+    </h2>
+
+    <div class="espacadorTitulo"></div>
+
+    <div class="ui stackable sixteen column grid espacoColunas">
+        <div class="column"></div>
+
+
+         <div class="eight wide column">
+        	 <div class="ui center aligned piled segment">
+                  <h2 class="ui center aligned header tituloSegmento">Informações</h2>
+                  
+                  <div class="ui divider"></div>
+                
+                <h4 class="ui header">
+                <i class="book icon"></i>
+                <div class="content">
+                    Nome Científico: <em>Malus domestica.</em>
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="globe icon"></i>
+                <div class="content">
+                    Nome Popular: Macieira.
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="map signs icon"></i>
+                <div class="ui left aligned content">
+                    Bioma: Locais com clima temperado, solo sílico-argiloso e profundo.
+
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="map marker alternate icon"></i>
+                <div class="ui left aligned content">
+                    Ocorrência:  Santa Catarina, Rio Grande do Sul, Paraná, e São Paulo. 
+                </div>
+                </h4>
+
+                <h4 class="ui header">
+                <i class="lemon icon"></i>
+                <div class="ui left aligned content">
+                   Frutífera: Sim. 
+                </div>
+                </h4>
+
+            </div>
+        </div>
+
+        <div class="column"></div>
+
+         <div class="five wide column">
+            <div class="ui piled segment imagemDetalha">
+                <img class="ui huge image imagemDetalha" src="imagens/catalogo/macieira.jpg"/>  
+            </div>
+
+        </div>
+
+        <div class="column"></div>
+        
+	</div>
+
+	<div class="divisorDetalha"></div>
+
+    <div class="ui sixteen column grid">
+        
+        <div class="column"></div>
+
+        <div class="six wide column">
+            <div class="ui styled fluid accordion">
+                <div class="title active">
+                    <i class="dropdown icon"></i>
+                    Quais as diferenças de cores?
+                </div>
+                <div class="content active">
+                    <p class="transition visible textoJustificado" style="display: block !important;">Flores brancas ou róseas, bem aromáticas. Já o caule é em tom pardo</p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    Quais as cidades que utilizam esta árvore para urbanismo?
+                </div>
+                <div class="content">
+                    <p class="transition hidden">Sem maiores informações.</p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    As flores aparecem em qual época?
+                </div>
+                <div class="content">
+                    <p class="transition hidden">São encontradas flores em Novembro, mas as frutas são colhidas entre fevereiro e maio </p>
+                </div>
+                <div class="title">
+                    <i class="dropdown icon"></i>
+                    Onde Comprar
+                </div>
+                <div class="content">
+                    <p class="transition hidden">Na frutaria mais próxima.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="eight wide column">
+	    	<div class="ui segment">
+			    <div class="ui comments">
+				  <div class="comment">
+				    <a class="avatar imagemUsuario">
+				      <img src="imagens/detalhaArvore/avatar3.jpg">
+				    </a>
+				    <div class="content">
+				      <a class="author">Kylie</a>
+				      <div class="metadata">
+				        <div class="date">Hoje</div>
+				      </div>
+				      <div class="text">
+				        Sempre que vejo esta árvore me encanto com a sua beleza.
+				      </div>
+				      <div class="actions">
+				        <a class="reply active">Responder</a>
+				      </div>
+				      <form class="ui reply form">
+				        <div class="field">
+				          <textarea></textarea>
+				        </div>
+				        <div class="ui primary submit labeled icon button botaoEnviar">
+				          <i class="icon edit"></i> Adicionar Resposta
+				        </div>
+				      </form>
+				    </div>
+				  </div>
+				</div>
+			</div>
+		</div>
+
+        <div class="column"></div>
+
+    </div>
+
+
+    <div class="espacadorInferior"></div> 
+
+<?php
+    include 'html/rodape.html';
+?>
