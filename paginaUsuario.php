aad37197a4c5a8717e8221379a4e5c597e458f12
Victor Hugo <victorhugonass@gmail.com>
2018-03-05 21:28:30 -0300

    criação da página de usuário(já que a detalha árvore ainda não fora terminada), sinto uma certa falta de alinhamento entre os elementos, então esta página ainda poderá sofrer alterações

---
+++ paginaUsuario.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Standard Meta -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link rel="stylesheet" href="css/semantic/semantic.css">
+  <link rel="stylesheet" href="css/victor.css">
+  <link rel="stylesheet" href="css/loginPaginaUnica.css" >
+  <link rel="shortcut icon" type="image/png" href="imagens/favicon.png"/>
+  <!-- Site Properties -->
+  <title>Página de Usuário</title>
+
+</head>
+<body id="fundoLoginU">
+
+<button id="botaoVoltar" class=" ui button"><a id="corBotao" href="index.php"><i class="arrow left icon"></i>Voltar</a></button>
+
+    <div id="card" class="ui card ">
+        <div class="image">
+            <img src="imagens/user.png">
+        </div>
+        <div class="content">
+            <a class="header">Username</a>
+            <div class="meta">
+                <span class="date">E-mail</span>
+            </div>
+            <div class="description">
+                Descrição básica
+            </div>
+        </div>
+        <button class="ui icon button">
+            <i class="edit icon"></i> Editar perfil
+        </button>
+    </div>
+
+</body>
+
+</html>
