7732c5cd6377f7db5e6309d6d166789fa51b8d20
Pedro Gabriel Gabiatti Guardini <pgabiattiguardini@gmail.com>
2018-03-06 22:27:50 -0300

    Accordion funcionando

---
+++ detalhaLichia.php
@@ -0,0 +1,150 @@
+<?php
+
+    include "html/cabecalho.html";
+
+?>
+
+<div class="espacadorTitulo"></div>
+
+<h2 class="ui horizontal divider header tituloPag">
+    Lichieira
+</h2>
+
+<div class="espacadorTitulo"></div>
+
+<div class="ui stackable sixteen column grid espacoColunas">
+    <div class="column"></div>
+
+
+     <div class="eight wide column">
+         <div class="ui center aligned piled segment">
+              <h2 class="ui center aligned header tituloSegmento">Informações</h2>
+              
+              <div class="ui divider"></div>
+            
+            <h4 class="ui header">
+            <i class="book icon"></i>
+            <div class="content">
+                Nome Científico: <em>Litchi chinensis</em>
+            </div>
+            </h4>
+
+            <h4 class="ui header">
+            <i class="globe icon"></i>
+            <div class="content">
+                Nome Popular: Lichieira
+            </div>
+            </h4>
+
+            <h4 class="ui header">
+            <i class="map signs icon"></i>
+            <div class="ui left aligned content">
+                Bioma: Planícies aluviais de beira de rios, Baixadas da mata pluvial e Submatas de altitude, principalmente de pinhais.
+            </div>
+            </h4>
+
+            <h4 class="ui header">
+            <i class="map marker alternate icon"></i>
+            <div class="ui left aligned content">
+                Ocorrência: É natural das regiões quentes da Ásia, sendo encontrada principalmente na República Popular da China, Índia, Madagáscar, Nepal, Bangladesh, Paquistão, sul e centro de Taiwan, a norte do Vietname, Indonésia, Tailândia, Filipinas, África do Sul e México.
+            </div>
+            </h4>
+
+            <h4 class="ui header">
+            <i class="lemon icon"></i>
+            <div class="ui left aligned content">
+               Frutífera: Sim. O fruto da lichieira é uma drupa, com polpa translúcida (arilo), normalmente codiforme ou oval e disposto em cachos. A casca é vermelho-brilhante (quando maduro), delgada, coriácea e quebradiça.mA polpa é branca, rica em vitamina C, potássio, cálcio, fósforo e ferro. 
+            </div>
+            </h4>
+
+        </div>
+    </div>
+
+    <div class="column"></div>
+
+     <div class="five wide column">
+        <div class="ui piled segment imagemDetalha">
+            <img class="ui huge image imagemDetalha" src="imagens/catalogo/jabuticaba.jpg"/>  
+        </div>
+
+    </div>
+
+    <div class="column"></div>
+    
+</div>
+
+<div class="divisorDetalha"></div>
+
+<div class="ui sixteen column grid">
+    
+    <div class="column"></div>
+
+    <div class="six wide column">
+        <div class="ui styled fluid accordion">
+            <div class="title active">
+                <i class="dropdown icon"></i>
+                Quais são os fins medicinais da Jabuticaba?
+            </div>
+            <div class="content active">
+                <p class="transition visible textoJustificado" style="display: block !important;">A jabuticaba é utilizada para vários fins, tanto culinários como medicinais. Entre estes, o chá obtido com a casca do fruto é usado como tratamento para diarreias e disenterias, bem como para tratamento de inflamações crônicas nas amídalas, em que se deve fazer gargarejos com esse chá.</p>
+                <p class="transition visible textoJustificado" style="display: block !important;">Entre outras propriedades fitoterápicas, destacam-se antiasmática, hemoptise, inflamações dos intestinos. êm uma potente ação antioxidante, ou seja, ajudam a eliminar do organismo moléculas instáveis de radicais livres. Esse efeito, observado em tubos de ensaio, dá uma pista para se compreender porque a incidência de tumores e problemas cardíacos é menor entre consumidores de alimentos ricos no pigmento.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Quais são os produtos feitos a partir dos frutos da Jabuticaba?
+            </div>
+            <div class="content">
+                <p class="transition hidden">.</p>
+            </div>
+            <div class="title">
+                <i class="dropdown icon"></i>
+                Como cuidar?
+            </div>
+            <div class="content">
+                <p class="transition hidden">.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="eight wide column">
+        <div class="ui segment">
+            <div class="ui comments">
+              <div class="comment">
+                <a class="avatar imagemUsuario">
+                  <img src="imagens/detalhaArvore/avatar2.jpg">
+                </a>
+                <div class="content">
+                  <a class="author">Keyla</a>
+                  <div class="metadata">
+                    <div class="date">10 dias atrás</div>
+                  </div>
+                  <div class="text">
+                    É a minha fruta favorita! 
+                  </div>
+                  <div class="actions">
+                    <a class="reply active">Responder</a>
+                  </div>
+                  <form class="ui reply form">
+                    <div class="field">
+                      <textarea></textarea>
+                    </div>
+                    <div class="ui primary submit labeled icon button botaoEnviar">
+                      <i class="icon edit"></i> Adicionar Resposta
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="column"></div>
+
+</div>
+
+
+<div class="espacadorInferior"></div> 
+
+<?php
+
+    include "html/rodape.html";
--- sobre.php
+++ sobre.php
@@ -4,8 +4,8 @@
   <h1 class="ui header centered">Sobre CulTrees</h1>
 
     <div class="ui piled segment">
-        <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem aliquip detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque electram, eos choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
-        <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo. Vis sint solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide phaedrum, vim vivendum maiestatis in.</p>
+        <p>CulTrees é uma empresavoltada ao ramo de classificação e estudo de árvores e plantas de todo o país.Foi fundada em 2018 por três jovens programadores.</p>
+        <p>Hoje o CulTrees envolve mais de 100 colaboradores, sendo biólogos ou amantes da natureza.Nossa equipe trata de ter todas suas informações verídicas e aprovadas por biólogos renomados.</p>
         <p>Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores adolescens, no illum aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo. Populo quodsi te eam, wisi everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu per, quas minimum postulant per id.</p>
     </div>
     <div class="ui container">
@@ -66,6 +66,11 @@
             </div>
         </div>
 
+    </div>
+    <div class="ui basic modal">
+        <div class="image content">
+                <img class="ui massive image imgSobre" src="imagens/forest.jpg">
+        </div>
     </div>
     <div class='clear'></div>
 
