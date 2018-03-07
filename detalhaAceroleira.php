0cfc3cf0814d1a1ff6702d9d73b62538dbb31b82
Victor Hugo <victorhugonass@gmail.com>
2018-03-06 22:08:12 -0300

    detalhamento das árvores completo

--- detalhaAceroleira.php
+++ detalhaAceroleira.php
@@ -5,7 +5,7 @@
     <div class="espacadorTitulo"></div>
 
     <h2 class="ui horizontal divider header tituloPag">
-        Pau Formiga
+        Aceroleira
     </h2>
 
     <div class="espacadorTitulo"></div>
@@ -23,35 +23,35 @@
                 <h4 class="ui header">
                 <i class="book icon"></i>
                 <div class="content">
-                    Nome Científico: <em>Triplaris americana</em>
+                    Nome Científico: <em>Malpighia glabra.</em>
                 </div>
                 </h4>
 
                 <h4 class="ui header">
                 <i class="globe icon"></i>
                 <div class="content">
-                    Nome Popular: Pau-formiga
+                    Nome Popular: Aceroleira, cereja-das-antilhas.
                 </div>
                 </h4>
 
                 <h4 class="ui header">
                 <i class="map signs icon"></i>
                 <div class="ui left aligned content">
-                    Bioma: Locais de clima Equatorial, Subtropical e Tropical
+                    Bioma: Locais de Tropical.
                 </div>
                 </h4>
 
                 <h4 class="ui header">
                 <i class="map marker alternate icon"></i>
                 <div class="ui left aligned content">
-                    Ocorrência:  região amazônica e Centro-Oeste do Brasil.
+                    Ocorrência: em todo o território nacional se encontra facilmente.
                 </div>
                 </h4>
 
                 <h4 class="ui header">
                 <i class="lemon icon"></i>
                 <div class="ui left aligned content">
-                   Frutífera: Não. 
+                   Frutífera: Sim, uma baga vermelha de até 2,5 cm com 2/3 sementes, de polpa acidulada. 
                 </div>
                 </h4>
 
@@ -62,7 +62,7 @@
 
          <div class="five wide column">
             <div class="ui piled segment imagemDetalha">
-                <img class="ui huge image imagemDetalha" src="imagens/catalogo/pauformiga.jpg"/>  
+                <img class="ui huge image imagemDetalha" src="imagens/catalogo/acerola.jpg"/>  
             </div>
 
         </div>
@@ -81,29 +81,19 @@
             <div class="ui styled fluid accordion">
                 <div class="title active">
                     <i class="dropdown icon"></i>
-                    Quais as diferenças de cores?
+                    Como plantar acerola?
                 </div>
                 <div class="content active">
-                    <p class="transition visible textoJustificado" style="display: block !important;">Por ser dioica, possui plantas femininas e masculinas que se diferenciam nas cores da flor. A feminina, de rosa intenso, é vistosa em relação à masculina, mais apagada. Elas podem ser vistas de agosto a outubro.</p>
-                    <p class="transition visible textoJustificado" style="display: block !important;">Também conhecida por pau-de-novato, novateiro-de-mato-grosso ou formigueiro, esta árvore é empregada também no paisagismo, tendo em vista a beleza de suas flores. Apresenta preferência por solos muito úmidos ou alagados. De crescimento rápido, aos dois anos já atinge três metros de altura.</p>
+                    <p class="transition visible textoJustificado" style="display: block !important;">A aceroleira necessita de local ensolarado e clima tropical mas em regiões de clima mais ameno também pode ser cultivada.</p>
+                    <p class="transition visible textoJustificado" style="display: block !important;">O regime de chuvas precisa ser entre 1200 a 1600 mm anuais, bem distribuídas. Caso a irrigação com a água da chuva não seja suficiente, a aspersão com mangueiras ou gotejamento poderá suprir sua falta nos períodos de seca.</p>
                 </div>
                 <div class="title">
                     <i class="dropdown icon"></i>
-                    Quais as cidades que utilizam esta árvore para urbanismo?
+                    Pragas mais comuns?
                 </div>
                 <div class="content">
-                    <p class="transition hidden">.</p>
-                </div>
-                <div class="title">
-                    <i class="dropdown icon"></i>
-                    As flores aparecem em qual época?
-                </div>
-                <div class="content">
-                    <p class="transition hidden">.</p>
-                </div>
-                <div class="title">
-                    <i class="dropdown icon"></i>
-                    Onde Comprar
+                    <p class="transition hidden">As pragas mais comuns da cultura são pulgões, cochonilhas e nematóides. Para os dois primeiros, usar óleo de nim ou aqueles chás de plantas tóxicas, como os de folhas de alamanda.
+Para nematóides é um pouco mais difícil, as mudas devem ser certificadas de produção em solos não infestados por esta praga. Plantar tajetes (Tajete patula) ajuda no controle.</p>
                 </div>
             </div>
         </div>
@@ -113,15 +103,15 @@
 			    <div class="ui comments">
 				  <div class="comment">
 				    <a class="avatar imagemUsuario">
-				      <img src="imagens/detalhaArvore/avatar3.jpg">
+				      <img src="imagens/catalogo/oUserEscolheASuaPropriaFoto.jpg">
 				    </a>
 				    <div class="content">
-				      <a class="author">Kylie</a>
+				      <a class="author">George Miller</a>
 				      <div class="metadata">
-				        <div class="date">Hoje</div>
+				        <div class="date">3 semanas atrás</div>
 				      </div>
 				      <div class="text">
-				        Sempre que vejo esta árvore me encanto com a sua beleza.
+				        Frutinhas saborosas demais...
 				      </div>
 				      <div class="actions">
 				        <a class="reply active">Responder</a>
